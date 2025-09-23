f2ea69c65cb34ca1fa5cf844be2136289233ac6c
preynibos <stokprey5@gmail.com>
2025-09-23 15:14:20 +0700

    filephp

---
+++ operator.php
@@ -0,0 +1,9 @@
+<?php 
+$x = 10; 
+$y = 6; 
+
+$a = "Hello"; 
+$b = $a . " Guys "; 
+echo $b . "<br>";  
+echo $a . " Salam Kenal " . " Prey Nih Bos "; 
+?> 
---
+++ string1.php
@@ -0,0 +1,20 @@
+<?php 
+$string1 = 'Hallow Guys'; 
+
+$string2 = 'Saya Prey berasal dari Situbondo Kota Santri'; 
+
+$string3 = 'I Say: "\'Situbondo Nih Genk"'; 
+
+$string4 = 'Gacorin Serrrr Lekkk'; 
+
+$string5 = 'Good'; 
+
+$string6 = 'Bye Guys'; 
+
+echo $string1 . "<br>";  
+echo $string2 . "<br>";  
+echo $string3 . "<br>";  
+echo $string4 . "<br>";  
+echo $string5 . "<br>";  
+echo $string6; 
+?> 
